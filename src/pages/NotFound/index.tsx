be60bf07e143eee709fb612e8c836ae07539f187
Nika Sakandelidze <nikasakana@gmail.com>
2023-11-16 23:34:13 +0400

    Added not found page utilities

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,23 @@
+import { Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export const NotFound = () => {
-  return <div>Not found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Grid container display="flex" justifyContent="center">
+      <Typography variant="h3">Page Not Found &nbsp;</Typography>
+      <Typography
+        variant="h3"
+        sx={{
+          textDecoration: "underline",
+          cursor: "pointer",
+          "&:hover": { color: (theme) => theme.palette.primary.main },
+        }}
+        onClick={() => navigate("/")}
+      >
+        Go to home page?
+      </Typography>
+    </Grid>
+  );
 };
